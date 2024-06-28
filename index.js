89dbfce7dbc1c57369944fe90810b61645aa1b9e
FRANK7434 <sonfransiscojames015@gmail.com>
2024-06-28 20:35:51 +0300

    added arithmetic -lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply)
+let random = Math.floor(Math.random() * 20) + 1;
+const num3 = 60;
+const num4= 7;
+let mod = (num3 % num4 === 4) ? 4 : num3 % num4;
+ console.log (mod)
+ const numbers=( 2,4,7,20,8,10,11)
+ let max=Math.max(2,3,20,8,9)
+ console.log(max)
